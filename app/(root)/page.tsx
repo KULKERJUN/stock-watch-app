6cb95ca4f192ebe2d043661881814ab04a238a15
Ahnaf Iqbal <md.ahnaf.iqbal@g.bracu.ac.bd>
2025-12-06 05:37:58 +0600

    Header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+
+const Home: () => Element = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            Home
+        </div>
+    );
+}
+
+export default Home;
+
